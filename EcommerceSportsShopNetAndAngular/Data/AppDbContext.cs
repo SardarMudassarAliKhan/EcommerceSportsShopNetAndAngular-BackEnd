f6f1bfb95275128c59ed8a59e6d1cf086bce8442
mudassarali.official <mudassarali.official@gmail.com>
2024-03-03 00:47:12 +0500

    ECommerce Sports Shop 2024 Angular & .NET Core

---
+++ EcommerceSportsShopNetAndAngular/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EcommerceSportsShopNetAndAngular.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceSportsShopNetAndAngular.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ EcommerceSportsShopNetAndAngular/Program.cs
@@ -0,0 +1,41 @@
+using EcommerceSportsShopNetAndAngular.Data; // Make sure to import the namespace for AppDbContext
+using Microsoft.EntityFrameworkCore; // Make sure to import the namespace for Entity Framework Core
+
+namespace EcommerceSportsShopNetAndAngular
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
